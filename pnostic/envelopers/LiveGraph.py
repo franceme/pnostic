ed975b6c5c94168766ab467b1f92593f21991645
frantzme <frantzme@vt.edu>
2023-10-28 03:05:05 +0000

    Added a single ez enveloper

--- pnostic/envelopers/LiveGraph.py
+++ pnostic/envelopers/LiveGraph.py
@@ -4,11 +4,21 @@ from pnostic.structure import Envelop, RepoResultObject, RepoObject, RepoSifting
 from pnostic.utils import now
 
 class app(Envelop):
+    """
+    https://www.pyvibe.com/flask.html
+
+    https://github.com/pycob/pyvibe
+
+    https://stackoverflow.com/questions/31264826/start-a-flask-application-in-separate-thread
+
+    https://www.pyvibe.com/gallery/chart.html
+    """
     def __init__(self, ipaddress:str="127.0.0.1", port_to_use:int=5000):
         super().__init__()
         self.imports = [
             "pyvibe",
-            "flask"
+            "flask",
+            "plotly"
         ]
         self.flask_app = None
         self.flask_runner_thread = None
@@ -32,6 +42,80 @@ class app(Envelop):
         def index():
             page = pv.Page('Home')
             page.add_header('Hello World')
+            with page.add_card() as card:
+                card.add_header("Load the graph")
+                card.add_link("Learn more", "http://{0}:{1}/graph".format(self.host, self.port))
+            return page.to_html()
+
+        @self.flask_app.route("/graph")
+        def graph():
+            """
+            https://www.pyvibe.com/gallery/chart.html
+            """
+            import pyvibe as pv
+            from pyvibe import HtmlComponent as html
+            import plotly.express as px
+
+            df = mystring.frame.from_arr([{
+                "Runner": "Cryptolation",
+                "NumberProjects": 10 
+            }, {
+                "Runner": "Semgrep",
+                "NumberProjects": 4 
+            }])
+
+            #https://stackoverflow.com/questions/31638439/remove-highlight-from-active-button
+
+            page = pv.Page("Number of Repo Objects Scanned")
+            page.add_header("Chart")
+
+
+            page.add_html("""
+<style>
+.mystyle {
+  width: 100%;
+  padding: 25px;
+  background-color: coral;
+  color: white;
+  font-size: 25px;
+  box-sizing: border-box;
+}
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script>
+     var time = new Date().getTime();
+     $(document.body).bind("mousemove keypress", function(e) {
+         time = new Date().getTime();
+     });
+
+     function refresh() {
+         if(new Date().getTime() - time >= 60000 && document.getElementById("myDIV").innerHTML != "Stopped Reloading") 
+             window.location.reload(true);
+         else 
+             setTimeout(refresh, 10000);
+     }
+
+     setTimeout(refresh, 10000);
+</script>
+
+<script>
+function myFunction() {
+   var element = document.getElementById("myDIV");
+   element.classList.toggle("mystyle");
+   if (element.innerHTML == "") {
+    element.innerHTML = "Stopped Reloading";
+   } else {
+    element.innerHTML = "";
+   }
+}
+</script>
+<button onclick="myFunction()">Start refreshing</button>
+<div id="myDIV"></div>
+""")
+
+            fig = px.bar(df, x='Runner', y='NumberProjects')
+            page.add_plotlyfigure(fig)
+
             return page.to_html()
         
         self.flask_app.run(host=self.host, port=self.port, debug=True,use_reloader=False)
