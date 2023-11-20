a27e801743fda5e8da4d97e50ee423c9ef2ffe78
franceme <frantzme@vt.edu>
2023-11-20 12:05:49 -0500

    fix issue

--- pnostic/structure.py
+++ pnostic/structure.py
@@ -690,7 +690,7 @@ with utils.clean_op_env():
 
         self.assets = ["pnostic"]
 
-        self.seclusion_env = seclusion_env or core_seclusion
+        self.seclusion_env = seclusion_env
 
         for big_list in (fileProviders + runners + loggersset + enveloperset + [self.seclusion_env]):
             if isinstance(big_list, list):
